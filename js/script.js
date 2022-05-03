6105aeaed9dac035b3f9e13a6a67cb25144c66e1
Guidocode <guidobuono@icloud.com>
2022-05-03 16:42:52 +0200

    Rendo dinamico il contenuto

---
+++ js/script.js
@@ -0,0 +1,83 @@
+/*
+    Descrizione:
+    Partendo dal markup consegnato rendere dinamici tutti i contenuti 
+    (foto e testi) e al click su una thumb, visualizzare in grande 
+    l’immagine corrispondente.
+
+    Bonus:
+    1- applicare l’autoplay allo slider: ogni 3 secondi, cambia 
+    immagine automaticamente
+    2- quando il mouse va in hover sullo slider, bloccare 
+    l’autoplay e farlo riprendere quando esce
+*/
+
+
+/*
+    1. Rendo dinamico il contenuto
+    2. Al click sulla thumb visualizzo in item l'immagine corrispondente
+*/
+
+
+const app = new Vue ({
+    
+    el: '#app',
+
+    data: {
+
+        slides: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ],
+
+        counterSlide: 0,
+    },
+
+    methods:{
+
+        nextSlide(){
+
+            this.counterSlide++;
+
+            if(this.counterSlide > this.slides.length -1){
+                this.counterSlide = 0;
+            }
+        },
+
+        prevSlide(){
+
+            this.counterSlide--;
+
+            if(this.counterSlide < 0){
+                this.counterSlide = this.slides.length -1
+            }
+        }
+    }
+
+
+    
+    
+    
+})
